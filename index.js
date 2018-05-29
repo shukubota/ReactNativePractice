ac57b8c12398527846ac9c781adcdc60991a50b7
shukubota <kubotashu0421@gmail.com>
2018-05-29 14:32:10 +0900

    index修正

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import { AppRegistry } from 'react-native';
+todolist/index.js
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import { Provider } from 'react-redux'
+import store from './app/store'
 
 // MainContainerを利用するため
-import App from './app/containers/Main';
+import MainComponent from './app/components/Main'
+
+/**
+ * Provideを使ってreduxとreact連携する
+ */
+const App = () => {
+  return (
+    <Provider store={store}>
+      <MainComponent />
+    </Provider>
+  )
+}
+
 
 AppRegistry.registerComponent('ReactNativePractice', () => App);
