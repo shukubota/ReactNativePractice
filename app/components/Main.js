ed90ba778b588d94ef3181e6b43f1c65f8b94505
shukubota <kubotashu0421@gmail.com>
2018-05-29 15:16:33 +0900

    componentとcontainers

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,46 +1,34 @@
 // 必要なリソース追加する
 import React, { Component } from 'react';
 import {
-  Text,
   View,
-  FlatList,
-  TextInput,
   StyleSheet,
-  Platform,
+  Platform
 } from 'react-native';
+
+import VisibleTodoList from '../containers/VisibleTodoList'
+import AddTodo from '../containers/AddTodo'
 
 class MainContainer extends Component {
   render() {
-    // データソース定義
-    let todos = [
-      { id: 1, title: "create some actions" },
-      { id: 2, title: "create some reducer" },
-      { id: 3, title: "create store" }
-    ];
     // この部分はビューをレンダーです。
     return (
       <View style={styles.container}>
-        <FlatList data={todos}
-                  renderItem={this._renderItems}
-                  keyExtractor={(item, index) => index} />
+        <VisibleTodoList />
+        <AddTodo />
       </View>
     );
-  }
-
-　// このメソッドでは一覧の各アイテムをレンダーするです。
-  _renderItems({ item }) {
-    return (
-      <View>
-        <Text>{item.title}</Text>
-      </View>
-    )
   }
 }
 
 // ビューのスタイル修正
 const styles = StyleSheet.create({
   container: {
-    marginTop: Platform.OS === "ios" ? 20 : 0
+    marginTop: Platform.OS === "ios" ? 20 : 0,
+    flex: 1,
+    justifyContent: "space-between",
+    flexDirection: "column",
+    backgroundColor: "#1D9FF2"
   }
 });
 
